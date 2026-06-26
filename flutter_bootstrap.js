65d54fd7b09509644011e77ea36290660261014d
UXUI.Nattapong Teeropast <uxui.nattapongteeropast@MacBook-Air-khxng-UXUINattapong.local>
2026-06-26 13:29:28 +0700

    deploy: font switcher in display settings

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "221210742"
+    serviceWorkerVersion: "3841672001"
   }
 });
