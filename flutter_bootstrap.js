c7db1b1d514ad58e7f955d325285bd37c60c0a90
UXUI.Nattapong Teeropast <uxui.nattapongteeropast@MacBook-Air-khxng-UXUINattapong.local>
2026-07-21 15:44:22 +0700

    deploy: BMS badge on profile avatar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1126744679"
+    serviceWorkerVersion: "2562495034"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6c50b104d26701ba1ea515b831e51975",
+const RESOURCES = {"flutter_bootstrap.js": "8bf313e2f9b358d9f429e0d2708880b7",
 "version.json": "797e31383cd3b8236a7a4d92c64ee20a",
 "index.html": "5493ea5761d45adae933990402364b90",
 "/": "5493ea5761d45adae933990402364b90",
-"main.dart.js": "467c94ff61589e6134f8d8aaae55a2a1",
+"main.dart.js": "884ab10539d57b9c99158ddc3888b21c",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
